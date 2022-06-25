2a91782e64140630fb00657a52d4a863f33017bf
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2022-06-25 15:04:09 -0500

    Cast mulitple walls

--- Boundary.js
+++ Boundary.js
@@ -1,13 +1,12 @@
-class Point {
-  constructor(x, y) {
-    this.point = createVector(x, y);
-  }
-}
-
 class Boundary {
-  constructor(start, end) {
-    this.pA = start.point;
-    this.pB = end.point;
+  constructor(x1, y1, x2, y2) {
+    this.pA = createVector(x1, y1);
+    this.pB = createVector(x2, y2);
+  }
+
+  randomize() {
+    this.pA.set(random(width), random(height));
+    this.pB.set(random(width), random(height));
   }
 
   show() {
--- sketch.js
+++ sketch.js
@@ -1,29 +1,34 @@
-let wall;
-
-let pStart;
-let pEnd;
+let walls = [];
 
 let ray;
+let actor;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 
-  pStart = new Point(300, 100);
-  pEnd = new Point(300, 300);
-  wall = new Boundary(pStart, pEnd);
-  ray = new Ray(100, 200);
+  for (let i = 0; i < 5; i++) {
+    let x1 = random(width);
+    let y1 = random(height);
+    let x2 = random(width);
+    let y2 = random(height);
+    walls[i] = new Boundary(x1, y1, x2, y2);
+  }
+  walls.push(new Boundary(0, 0, 0, height)); //left vertical boundary
+  walls.push(new Boundary(width, 0, width, height)); //right vertical boundary
+  walls.push(new Boundary(0, 0, width, 0)); //Upper horizontal boundary
+  walls.push(new Boundary(0, height, width, height)); //Lower horizontal boundary
+
+  actor = new Actor();
 }
 
 function draw() {
   background(0);
-  wall.show();
-  ray.show();
-  ray.lookAt(mouseX, mouseY);
 
-  let pt = ray.cast(wall);
-
-  if (pt) {
-    fill(255);
-    ellipse(pt.x, pt.y, 8, 8);
+  for (let wall of walls) {
+    wall.show();
   }
+
+  actor.update(mouseX, mouseY);
+  actor.show();
+  actor.emanate(walls);
 }
